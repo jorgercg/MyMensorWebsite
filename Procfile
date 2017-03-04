363af85f4bd508c2b9abb6c5aac7093dfbf7d64a
Jorge Goncalves <jrcgonc@gmail.com>
2017-03-04 18:01:44 +0000

    04MAR2017 18h01min

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: waitress-serve --port=$PORT mymwebsitesett.wsgi:application
---
+++ mymwebsitesett/settings_production.py
@@ -0,0 +1,20 @@
+from mymwebsitesett.settings import *
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+
+# UPDATE BEFORE LAUNCH !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+ALLOWED_HOSTS = ['*']
+
+DEBUG = True
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
