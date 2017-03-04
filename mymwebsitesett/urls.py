c5ce52db1f2ae6af0f7c17426adc8cd36dfa052f
Jorge Goncalves <jrcgonc@gmail.com>
2017-03-04 18:22:47 +0000

    04MAR2017 18h22min

--- mymwebsitesett/urls.py
+++ mymwebsitesett/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from mymwebsite import mymviews
 
 urlpatterns = [
+    url(r'^$', mymviews.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
