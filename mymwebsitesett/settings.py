70a1955148c4fe4b356d6e18c95c8f1dcb85dbce
Jorge Goncalves <jrcgonc@gmail.com>
2017-06-02 12:51:18 +0100

    02JUN2017 12h51min

--- mymwebsitesett/settings.py
+++ mymwebsitesett/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'cbm&j+b3ipqzn4notw9so#8e@e54upnr)s709nijgq!_o-d(+u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['www.mymensor.com','mymensor.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -123,5 +123,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = 'mymwebsite/static'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'mymwebsite/static'),)
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'mymwebsitesett/static'),)
