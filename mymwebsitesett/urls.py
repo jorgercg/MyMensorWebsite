d74d8ffaf6ced28deff10d9390da392bdf759b8c
Jorge Goncalves <jrcgonc@gmail.com>
2017-11-15 18:45:50 +0000

    15NOV2017 18h45min

--- mymwebsitesett/urls.py
+++ mymwebsitesett/urls.py
@@ -40,9 +40,11 @@ urlpatterns += i18n_patterns(
     url(r'^cases/genusemediaplan/$', TemplateView.as_view(template_name="cases/genusemediaplan.html"), name='genusemediaplan'),
     url(r'^cases/followingjobs/$', TemplateView.as_view(template_name="cases/followingremotejobs.html"), name='followingremotejobs'),
     url(r'^cases/usingyourowndata/$', TemplateView.as_view(template_name="cases/usingyourowndata.html"), name='usingyourowndata'),
+    url(r'^cases/salperctrl/$', TemplateView.as_view(template_name="cases/salperctrl.html"), name='salpercrtl'),
     url(r'^howto/$', mymviews.howto, name='howto'),
     url(r'^howto/genusemediaplan/$', TemplateView.as_view(template_name="howto/genusemediaplan.html"),name='genusemediaplanhowto'),
     url(r'^howto/followingjobs/$', TemplateView.as_view(template_name="howto/followingremotejobs.html"),name='followingremotejobshowto'),
     url(r'^howto/usingyourowndata/$', TemplateView.as_view(template_name="howto/usingyourowndata.html"),name='usingyourowndatahowto'),
+    url(r'^howto/salperctrl/$', TemplateView.as_view(template_name="howto/salperctrl.html"), name='salpercrtlhowto'),
 
 )
